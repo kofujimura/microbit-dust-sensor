5f5ec7151629ddf6238725c23c97bceff8fa6658
Ko Fujimura <fujimura.ko@gmail.com>
2021-05-02 16:34:39 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,14 @@
 let duration = 0
 let lowpulseoccupancy = 0
 let timeframe = 0
+basic.showIcon(IconNames.SmallDiamond)
 basic.forever(function () {
     duration = pins.pulseIn(DigitalPin.P0, PulseValue.Low)
     lowpulseoccupancy = lowpulseoccupancy + duration
-    if (timeframe > 1500) {
+    if (timeframe > 1300) {
         timeframe = 0
-        led.plotBarGraph(
-        lowpulseoccupancy / 1500,
-        2
-        )
-        soundExpression.giggle.play()
+        basic.showNumber(Math.trunc(lowpulseoccupancy / 300000))
+        basic.showIcon(IconNames.SmallDiamond)
         lowpulseoccupancy = 0
     }
     timeframe += 1
